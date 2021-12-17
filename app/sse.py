8e456b45ad077254d9de9980c931a70a949e9b5f
tim.kao <sk4920@columbia.edu>
2021-12-16 23:26:09 -0500

    backgound
    Signed-off-by: tim.kao <sk4920@columbia.edu>

--- app/sse.py
+++ app/sse.py
@@ -22,7 +22,7 @@ static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'statics')
 app = Flask(__name__, template_folder=tmpl_dir, static_folder=static_dir, static_url_path='')
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
-refreshRate = 60
+refreshRate = 180
 credentials_path = './pubsub/credential/myFile.privateKey.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 publisher = pubsub_v1.PublisherClient()
